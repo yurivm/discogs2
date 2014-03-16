4b1b6097185633eedd29ec8b78f79c2181efb8a4
yurivm <yuri.veremeyenko@gmail.com>
2014-03-16 23:19:28 +0100

    add the VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,18 @@ Bundler.setup
 
 require 'discogs2'
 require 'json'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   # some (optional) config here
+  #VCR
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  #config.extend VCR::RSpec::Macros
 end
 
