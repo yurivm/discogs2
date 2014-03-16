34a598eb2404b6d003826f6d64c75ccd01a79739
yurivm <yuri.veremeyenko@gmail.com>
2014-03-16 23:19:50 +0100

    copy the most request-response methods from the original discos (props to the author)

---
+++ lib/discogs2/request_methods.rb
@@ -0,0 +1,52 @@
+require 'uri'
+require 'net/http'
+require 'zlib'
+require 'stringio'
+
+module Discogs2
+  # mostly taken from the original discogs that handles request/response efficiently.
+  module RequestMethods
+    
+    # Queries the API and handles the response.
+    def query_api(path, params={})
+      response = perform_request(path, params)
+
+      raise "Unknown resource #{path}" if response.code == "404"
+      raise "Internal server error when querying #{path}" if response.code == "500"
+      # Unzip the response data, or just read it in directly
+      # if the API responds without gzipping.
+      response_body = nil
+      begin
+          inflated_data = Zlib::GzipReader.new(StringIO.new(response.body))
+          response_body = inflated_data.read
+      rescue Zlib::GzipFile::Error
+          response_body = response.body
+      end
+      response_body
+    end
+
+    def build_uri(path, params={})
+      querystring = "?" + params.map { |key, value| "#{key}=#{value}" }.sort.join("&")
+
+      URI.parse(File.join(api_host, URI.encode(sanitize_path(path, URI.escape(querystring)))))
+    end    
+
+    def sanitize_path(*path_parts)
+      clean_path = path_parts.map { |part| part.gsub(/\s/, '+') }
+      clean_path.join
+    end
+    
+    def perform_request(path, params={})
+      uri = build_uri(path, params)
+
+      request = Net::HTTP::Get.new(uri.path + "?" + uri.query)
+      request.add_field("Accept", "application/json")
+      request.add_field("Accept-Encoding", "gzip,deflate")
+      request.add_field("User-Agent", user_agent)
+
+      Net::HTTP.new(uri.host).start do |http|
+        http.request(request)
+      end
+    end    
+  end
+end
---
+++ spec/request_methods_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Discogs2::RequestMethods do
+  let(:test_class) {
+    Class.new do
+      include Discogs2::RequestMethods
+
+      def api_host
+        'http://api.discogs.com'
+      end
+    end
+  }
+  let(:test_instance) { test_class.new }
+  describe "#sanitize_path" do
+    it "replaces spaces with + in all the parts given and returns the joined string" do
+      expect(test_instance.sanitize_path("param", "some value", "one more space")).to eq("paramsome+valueone+more+space")
+    end
+    it "joins the arguments into a string with no replacement otherwise" do
+      expect(test_instance.sanitize_path("no", "replacements", "here")).to eq("noreplacementshere")
+    end
+  end
+  describe "#build_uri" do
+    it "builds a fully qualified url with parameters" do
+      expect(test_instance.build_uri("/path/to/resource", "param" => "value", "param2" => "value2").to_s).to eq("http://api.discogs.com/path/to/resource?param2=value2&param=value")
+    end
+    #not sure about this. should have been either a + or an encoded space %20.
+    xit "encodes spaces in params properly" do
+      expect(test_instance.build_uri("/path/to/resource", "param" => "value", "param2" => "value with spaces").to_s).to eq("http://api.discogs.com/path/to/resource?param2=value%2520with%2520spaces&param=value")
+    end
+  end
+
+  describe "#perform_request" do
+  end
+
+end
